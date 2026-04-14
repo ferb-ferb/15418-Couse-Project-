40caa2084671c35fb4b5a582e284c9d0667fd9de
Athan John Ferber <athanf@ghc28.ghc.andrew.cmu.edu>
2026-04-14 04:49:13 -0400

    uncomment wall thickness

--- src/container.h
+++ src/container.h
@@ -32,7 +32,7 @@ const float RECEIVER_CUP_DEPTH = 0.20f;
 // const float RECEIVER_CUP_WIDTH = 0.135f;
 // const float RECEIVER_CUP_HEIGHT = 0.12f;
 // const float RECEIVER_CUP_DEPTH = 0.15f;
-// const float CUP_WALL_THICKNESS = 0.025f;
+const float CUP_WALL_THICKNESS = 0.025f;
 
 // Set the cup positions
 const float SOURCE_CUP_CENTER_X = 0.14f;
